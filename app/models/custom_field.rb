f03e2a559490b17f1b9ed437d22891eec35ba524
Stephan Wiehr <stephan.wiehr@alpine.de>
2022-11-11 17:33:35 +0100

    feat: Add `before_validation` hook for custom fields

--- app/models/custom_field.rb
+++ app/models/custom_field.rb
@@ -298,6 +298,10 @@ class CustomField < ApplicationRecord
     nil
   end
 
+  def before_validation(custom_value)
+    format.before_validation(custom_value)
+  end
+
   # Returns the error messages for the given value
   # or an empty array if value is a valid value for the custom field
   def validate_custom_value(custom_value)
