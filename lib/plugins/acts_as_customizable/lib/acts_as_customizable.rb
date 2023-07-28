ef1fbe191b6f6c08e4c9045e667a4b2b48fc0886
Hare Ram Rai <hareram.rai@alpsalpine.com>
2023-07-28 13:26:53 +0200

    fix: Revert instanace level caching for available_custom_fields

--- lib/plugins/acts_as_customizable/lib/acts_as_customizable.rb
+++ lib/plugins/acts_as_customizable/lib/acts_as_customizable.rb
@@ -48,7 +48,7 @@ module Redmine
         end
 
         def available_custom_fields
-          @available_custom_fields ||= CustomField.where("type = '#{self.class.name}CustomField'").sorted
+          CustomField.where("type = '#{self.class.name}CustomField'").sorted.to_a
         end
 
         # Sets the values of the object's custom fields
