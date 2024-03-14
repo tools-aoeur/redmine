1751c6c972cdb16c6171a42360fbd5286ca2781b
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2024-03-14 13:49:02 +0100

    feat: Add shared query feature
    Notes:
    For all projects checkbox still exists (this kind of orthogonal to sharing)
    Queries could be executed by id although user is not allowed to see it (does not change issue visibility)

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -141,6 +141,7 @@ class QueriesController < ApplicationController
     else
       @query.visibility = Query::VISIBILITY_PRIVATE
     end
+    @query.sharing = params[:query] && params[:query][:sharing] || 'none'
     @query
   end
 
