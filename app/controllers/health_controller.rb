2eb4e5ffb784e6c9a1c193e683cf9f4b64ab5354
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2025-05-19 16:47:29 +0200

    fix: Simplify health check controller

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,6 +1,13 @@
-class HealthController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:check]
-  before_action :restrict_to_local, only: [:check]
+class HealthController < ActionController::Base
+  before_action :restrict_to_local, only: :check
+
+  # Perform a lightweight database connection check
+  def check
+    ActiveRecord::Base.connection.active?
+    render json: { status: 'ok' }, status: :ok
+  rescue ActiveRecord::ConnectionNotEstablished, ActiveRecord::NoDatabaseError
+    render json: { status: 'error', message: 'Database connection failed' }, status: :service_unavailable
+  end
 
   private
 
@@ -9,13 +16,5 @@ class HealthController < ApplicationController
     unless allowed_ips.include?(request.remote_ip)
       render json: { status: 'error', message: 'Access denied' }, status: :forbidden
     end
-  end
-
-  # Perform a lightweight database connection check
-  def check
-    ActiveRecord::Base.connection.active?
-    render json: { status: 'ok' }, status: :ok
-  rescue ActiveRecord::ConnectionNotEstablished, ActiveRecord::NoDatabaseError
-    render json: { status: 'error', message: 'Database connection failed' }, status: :service_unavailable
   end
 end
