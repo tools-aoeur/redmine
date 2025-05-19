ff17b2cf9e995a1ad0c9b63d90068184e4d33a2f
Jaap de Haan <jaap.dehaan@alpsalpine.com>
2025-05-19 13:57:41 +0200

    fix: Healthchecks without authentication from local call

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,4 +1,16 @@
 class HealthController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:check]
+  before_action :restrict_to_local, only: [:check]
+
+  private
+
+  def restrict_to_local
+    allowed_ips = ['127.0.0.1', '::1']
+    unless allowed_ips.include?(request.remote_ip)
+      render json: { status: 'error', message: 'Access denied' }, status: :forbidden
+    end
+  end
+
   # Perform a lightweight database connection check
   def check
     ActiveRecord::Base.connection.active?
