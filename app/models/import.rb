711ca4522055c4b96e96f68324c955c42956973d
Jaap de Haan <261428+jdehaan@users.noreply.github.com>
2025-10-24 08:01:19 +0000

    fix: Disable new rubocop rules not considered yet

--- app/models/import.rb
+++ app/models/import.rb
@@ -100,7 +100,8 @@ class Import < ApplicationRecord
       begin
         project = Project.find(options[:project_id])
         self.settings.merge!('mapping' => {'project_id' => project.id})
-      rescue; end
+      rescue
+      end
     end
   end
 
